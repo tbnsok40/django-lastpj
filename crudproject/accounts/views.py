47b58f210e51336655140e788017321c396cd1ca
lim <tbnsok40@naver.com>
2020-08-28 13:51:47 +0900

    [error] commentform not appear

--- crudproject/accounts/views.py
+++ crudproject/accounts/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User # Django가 제공해주는 User 모델을 사용하기 위해서 import 해줬습니다. 
+from django.contrib import auth # 그리고 인증 절차를 위해서도 가져왔습니다. 
+# Create your views here.
+
+
 def signup(request):
+    if request.method == 'POST': # 앞서 form에서 post 방식으로 요청을 보냈을 경우, 즉 회원가입 버튼을 눌렀을 경우 이 다음 과정이 실행됩니다. 
+        if request.POST['password1'] == request.POST['password2']: # 비밀번호가 일치하는지 확인합니다. 
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1']) # 홈에서 받은 아이디와 패스워드를 가진 유저를 생성합니다. 
+            auth.login(request, user) # 회원가입 후 바로 로그인이 되게 하기 위해 작성해주었습니다. 
+            return redirect('index') # 회원가입이 완료되면 index 페이지로 보냅니다. 
     return render(request, 'signup.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html',{'error':'username or password is wrong'})
+    else:
+        return render(request, 'login.html')
+
+
--- crudproject/blog/models.py
+++ crudproject/blog/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class Memo(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
-    
+    #today = timezone.now() #새미누나 코드
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Memo,on_delete=models.CASCADE)
+    body = models.CharField("내용", max_length=250)
+    create_at = models.DateTimeField('작성시간', default = timezone.now)
+
+    def __str__(self):
+        return self.body
+    
